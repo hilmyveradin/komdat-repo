25946113d40f4faa24e11725886b7aedbbe0c41e
hilmyveradin <hilmyveradin@gmail.com>
2023-10-01 17:38:20 +0700

    modify script to enter username

--- initialize.sh
+++ initialize.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Install required dependencies
+# Prompt the user for their username
+read -p "Enter your username: " USERNAME
+
+echo "You entered the username: $USERNAME"
+
 sudo apt-get update
 
-##Install SSH
+## Install SSH
 sudo apt-get install ssh -y
 ## NVM and node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install 17.9.1
-## Docker (pre built in)
+## Docker (pre-built in)
 ## Nginx
 sudo apt install nginx
 
-# Clone the github repo
+# Clone the GitHub repo
 git clone https://github.com/hilmyveradin/focusstopwatch-vue.git
 
 # Move the directory to focusstopwatch-vue
-cd /home/ubuntu/focus-stopwatch
+cd /home/"$USERNAME"/focus-stopwatch
 
 # Run the docker container
 docker-compose up -d --build
