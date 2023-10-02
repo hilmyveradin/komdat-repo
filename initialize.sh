a7b2e90362f8568bc5d8a45824304862e36fa3c0
hilmyveradin <hilmyveradin@gmail.com>
2023-10-02 19:45:56 +0700

    update: update the docker and git installation

--- initialize.sh
+++ initialize.sh
@@ -9,10 +9,22 @@ sudo apt-get update
 
 ## Install SSH
 sudo apt-get install ssh -y
+
 ## NVM and node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install 17.9.1
-## Docker (pre-built in)
+
+## Install Docker
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+## Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 ## Nginx
 sudo apt install nginx
 
@@ -20,7 +32,7 @@ sudo apt install nginx
 git clone https://github.com/hilmyveradin/focusstopwatch-vue.git
 
 # Move the directory to focusstopwatch-vue
-cd /home/"$USERNAME"/focus-stopwatch
+cd focusstopwatch-vue
 
 # Run the docker container
 docker-compose up -d --build
